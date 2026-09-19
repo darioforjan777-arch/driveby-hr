d9efc429b33aa71026a8beec408cb5c62363a4aa
Dario <dario.forjan777@gmail.com>
2026-09-19 21:05:24 +0200

    Premjesti mobilni sadrzaj clanka prije prvog naslova

--- src/lib/portableText.ts
+++ src/lib/portableText.ts
@@ -25,7 +25,7 @@ interface PTImage {
   [key: string]: unknown;
 }
 
-function escapeHtml(s: string): string {
+export function escapeHtml(s: string): string {
   return s
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
